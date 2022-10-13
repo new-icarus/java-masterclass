beb09e0837a6f319cb52a65b0274ab276c019452
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-13 18:48:34 -0300

    byte, short, long and int max/min

---
+++ masterclass-sandbox/src/ByteShortIntLong.java
@@ -0,0 +1,19 @@
+public class ByteShortIntLong {
+    public static void main(String[] args) {
+
+        //Min and Max values of Int
+        System.out.println("Integer Minimum Value: " + Integer.MIN_VALUE +
+                "\nInteger Maximum Value: " + Integer.MAX_VALUE);
+
+        System.out.println("\nByte Minimum Value: " + Byte.MIN_VALUE +
+                "\nByte Maximum Value: " + Byte.MAX_VALUE);
+
+        System.out.println("\nShort Minimum Value: " + Short.MIN_VALUE +
+                "\nShort Maximum Value: " + Short.MAX_VALUE);
+
+        System.out.println("\nLong Minimum Value: " + Long.MIN_VALUE +
+                "\nLong Maximum Value: " + Long.MAX_VALUE);
+
+        long ultraLong = 2_147_483_648L; //use L to declare a long
+    }
+}
--- masterclass-sandbox/src/HelloWorld.java
+++ masterclass-sandbox/src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Tiago!");
+
+        int myFirstNumber = 5 + 15, mySecondNumber = 12, myThirdNumber = 6;
+
+        int myTotal = myFirstNumber +  mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 10000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
